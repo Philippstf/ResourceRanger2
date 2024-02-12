59eaf1884eba06d7eb3fc55e9d2f2306cd69ab5a
Philipp <wi21028@lehre.dhbw-stuttgart.de>
2024-02-12 23:09:10 +0100

    Eincheck / Auscheck Logik - Speicherung in JSON FILE mit Direktdownlaod

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,26 +10,32 @@ export class Tab2Page implements OnInit {
   timer: number = 0; // Timer als Anzahl der Sekunden
   timerDisplay: string = '00:00:00'; // Timer als formatierter String
   interval: any;
+  startTime: Date | null = null; // Deklariere startTime
+  endTime: Date | null = null; // Deklariere endTime
+  workSessions: any[] = [];
 
   constructor() {}
 
   ngOnInit() {}
 
   toggleCheckIn() {
-    if (this.isCheckingIn) {
-      // Auschecken
-      clearInterval(this.interval);
-      this.timer = 0; // Timer zurücksetzen
+  if (this.isCheckingIn) {
+    // Auschecken
+    this.endTime = new Date();
+    clearInterval(this.interval);
+    this.saveTime();
+    this.timer = 0;
+    this.updateTimerDisplay();
+  } else {
+    // Einchecken
+    this.startTime = new Date();
+    this.interval = setInterval(() => {
+      this.timer++;
       this.updateTimerDisplay();
-    } else {
-      // Einchecken
-      this.interval = setInterval(() => {
-        this.timer++;
-        this.updateTimerDisplay();
-      }, 1000);
-    }
-    this.isCheckingIn = !this.isCheckingIn;
+    }, 1000);
   }
+  this.isCheckingIn = !this.isCheckingIn;
+}
 
   updateTimerDisplay() {
     const hours = Math.floor(this.timer / 3600);
@@ -41,5 +47,38 @@ export class Tab2Page implements OnInit {
 
   pad(value: number) {
     return value.toString().padStart(2, '0');
+  }
+
+  saveTime() {
+    const currentDate = new Date().toLocaleDateString('de-DE');
+    const startTime = this.startTime?.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+    const endTime = this.endTime?.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+    const workTime = this.timerDisplay;
+  
+    // Neue Arbeitssitzung hinzufügen
+    this.workSessions.push({
+      Datum: currentDate,
+      Startzeit: startTime,
+      Endzeit: endTime,
+      Arbeitszeit: workTime
+    });
+  
+    // Arbeitssitzungen als JSON speichern
+    this.exportToJsonFile(this.workSessions);
+  }
+  
+  exportToJsonFile(jsonData: any) {
+    const jsonString = JSON.stringify(jsonData);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+  
+    // Erstelle einen Link zum Herunterladen der JSON-Datei
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'timetracker.json';
+    link.click();
+  
+    // URL freigeben
+    URL.revokeObjectURL(url);
   }
 }
